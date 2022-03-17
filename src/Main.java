b7519761da00abc940ac33a3c1edeb8d4b2e2f77
Riccardo Doveri <riccardo.doveri.1998@gmail.com>
2022-03-17 18:20:37 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import org.opencv.core.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        if (args[0].equals("vpoint")) {
+            System.out.println("Vanishing point detection: \n");
+
+            //load openCV library
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+            new VanishingPoint().run(args);
+        } else if (args[0].equals("circles")) {
+            System.out.println("Circles detection: \n");
+
+            //load openCV library
+            System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+            new Circle().run(args);
+        } else {
+            System.out.println("Incorrect parameters");
+        }
+    }
+}
